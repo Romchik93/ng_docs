b72535bd5937e39ddc62b740e5bbeaafddcc1362
Romchik93 <39699899+Romchik93@users.noreply.github.com>
2026-01-16 14:14:30 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,7 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+html_additional_pages = {"index": "index.html"}
 
 # -- Options for HTML output
 
